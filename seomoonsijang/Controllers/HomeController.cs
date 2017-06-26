dde42dd021349c18d4a4086b8726f2b668e4109b
HyoseopSong <csnvshs@naver.com>
2017-06-27 00:34:08 +0900

    Browser에서 Blob Storage로 파일 업로드 구현 완료

--- seomoonsijang/Controllers/HomeController.cs
+++ seomoonsijang/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace seomoonsijang.Controllers
 {
@@ -15,15 +20,36 @@ namespace seomoonsijang.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "서문시장의 건물별 위치와 이름을 확인하실 수 있습니다.";
 
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(HttpPostedFileBase file)
         {
-            ViewBag.Message = "Your contact page.";
 
+            if (file != null)
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("westgateproject_AzureStorageConnectionString"));
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("blob1");
+                
+                CloudBlockBlob blob = container.GetBlockBlobReference(file.FileName);
+                blob.UploadFromStream(file.InputStream);
+                ViewBag.Message = file.FileName;
+                               
+            }
+            else
+            {
+                ViewBag.Message = "파일 업로드가 실패했습니다!!";
+            }
+            
             return View();
         }
     }
