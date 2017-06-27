a1ca79e1835e9d2f5c7208736caf3036080de595
HyoseopSong <csnvshs@naver.com>
2017-06-27 11:42:14 +0900

    이전 커밋에서 전체적인 화면구성 손질함. 이번 커밋은 Table Storage에 text 저장하는 법 구현 완료, text 입력창 크기 조절 중 스벅 나가기로 해서 일단 커밋함

--- seomoonsijang/Controllers/HomeController.cs
+++ seomoonsijang/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
+using seomoonsijang.Models;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace seomoonsijang.Controllers
 {
@@ -31,23 +33,46 @@ namespace seomoonsijang.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Contact(HttpPostedFileBase file)
+        public ActionResult Contact(HttpPostedFileBase file, ContentsEntity contents)
         {
 
-            if (file != null)
+            if (file != null & contents != null)
             {
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("westgateproject_AzureStorageConnectionString"));
+
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer container = blobClient.GetContainerReference("blob1");
+                ViewBag.BlobSuccess = container.CreateIfNotExists();
+
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                CloudTable table = tableClient.GetTableReference("WestGateMarket");
+                ViewBag.TableSuccess = table.CreateIfNotExists();
+                contents.filename = file.FileName;
+                TableOperation insertOperation = TableOperation.Insert(contents);
+                TableResult result = table.Execute(insertOperation);
+                ViewBag.TableName = table.Name;
+                ViewBag.Result = result.HttpStatusCode;
                 
                 CloudBlockBlob blob = container.GetBlockBlobReference(file.FileName);
                 blob.UploadFromStream(file.InputStream);
                 ViewBag.Message = file.FileName;
                                
             }
+            else if(file == null & contents != null)
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("westgateproject_AzureStorageConnectionString"));
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                CloudTable table = tableClient.GetTableReference("WestGateMarket");
+                ViewBag.TableSuccess = table.CreateIfNotExists();
+                TableOperation insertOperation = TableOperation.Insert(contents);
+                TableResult result = table.Execute(insertOperation);
+                ViewBag.TableName = table.Name;
+                ViewBag.Result = result.HttpStatusCode;
+                ViewBag.Message = contents.PartitionKey + contents.RowKey + contents.text;
+            }
             else
             {
-                ViewBag.Message = "파일 업로드가 실패했습니다!!";
+                ViewBag.Message = "업로드할 내용이 없습니다.";
             }
             
             return View();
---
+++ seomoonsijang/Models/ContentsEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+namespace seomoonsijang.Models
+{
+    public class ContentsEntity : TableEntity
+    {
+        public ContentsEntity(string patKey, string rKey)
+        {
+            this.PartitionKey = patKey;
+            this.RowKey = rKey;
+        }
+
+        public ContentsEntity() { }
+
+        public string text { get; set; }
+        public string filename { get; set; }
+    }
+}
